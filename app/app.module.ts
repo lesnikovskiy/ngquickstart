d26c0d051e15f82eed6c869d1c0cbc2df72e4f76
lesnikovskiy <lesnikovski@gmail.com>
2016-09-28 14:51:05 +0300

    commit angular 2 quickstart

---
+++ app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+
+import { routing } from './app.routing';
+
+import { AppComponent }   from './app.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { DashboardComponent } from './dashboard.component';
+import { HeroService } from './hero.service';
+
+@NgModule({
+  imports:      [ 
+    BrowserModule,
+    FormsModule,
+    routing 
+  ],
+  providers: [HeroService],
+  declarations: [ 
+    AppComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent 
+  ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
